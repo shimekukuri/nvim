334ee8dff596e23a82228d1e9f5d40b2d74d420c
Tyler Hutchinson <jhutchinson192390@gmail.com>
2024-07-19 10:56:18 -0400

    updating

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,18 @@
+local telescope = require("telescope")
 local builtin = require('telescope.builtin')
+local open_with_trouble = require("trouble.sources.telescope").open
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = { ["<leader>m"] = open_with_trouble },
+    },
+  },
+})
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>m', builtin.diagnostics, {})
+--[[vim.keymap.set('n', '<leader>m', builtin.diagnostics, {})]] --
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
-
-

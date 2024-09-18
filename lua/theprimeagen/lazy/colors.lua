7c8655d13c7044664d8fa40c3c16d80979fea5da
shimekukuri <jhutchinson192390@gmail.com>
2024-09-18 01:21:54 +0000

    fixed background colors

--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -4,8 +4,8 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    --vim.api.nvim_set_h1(0, "NormalNC", { bg = "none" })
-    --vim.api.nvim_set_h1(0, "LineNr", { fg = "white" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = "white" })
 end
 
 return {
